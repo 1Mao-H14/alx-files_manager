3aa86e8cdd7b4afc027f33ebe4f83ead1d5b1296
marwan <marwanhamdaoui2020@gmail.com>
2025-02-11 12:22:57 -0800

    task  3

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+import routes from './routes/index';
 
-const server = express();
+const app = express();
+const port = process.env.PORT || 5000;
 
-injectMiddlewares(server);
-injectRoutes(server);
-startServer(server);
+// Load all routes
+app.use('/', routes);
 
-export default server;
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
