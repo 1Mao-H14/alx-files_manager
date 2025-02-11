917f734e65ceb18282d342fb5250b2ed07b02815
marwan <marwanhamdaoui2020@gmail.com>
2025-02-11 14:05:42 -0800

    task 2. First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,18 +2,35 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  static getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
-
-    res.status(200).json({ redis: redisAlive, db: dbAlive });
+  /**
+   * Controller for endpoint GET /status that retrieves
+   * mongodb client and redis client connection status
+   * @typedef {import("express").Request} Request
+   * @typedef {import("express").Response} Response
+   * @param {Request} _req - request object
+   * @param {Response} res  - response object
+   */
+  static getStatus(_req, res) {
+    if (dbClient.isAlive() && redisClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    }
   }
 
-  static async getStats(req, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-
-    res.status(200).json({ users: usersCount, files: filesCount });
+  /**
+   * Controller for endpoint GET /stats that retrieves
+   * count of users and files
+   * @param {Request} _req - Request object
+   * @param {Response} res  - Response object
+   * @param {import("express").NextFunction} next - Next function
+   */
+  static async getStats(_req, res, next) {
+    try {
+      const users = await dbClient.nbUsers();
+      const files = await dbClient.nbFiles();
+      res.status(200).json({ users, files });
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
